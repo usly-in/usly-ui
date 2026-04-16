e70dbf8f285153fb5607fed0d4aa185c34458eac
satya00089 <satya00089@users.noreply.github.com>
2026-04-16 12:45:50 +0000

    deploy: cfc6aaeba3391bdcbe5302b1db8f92882c6ac040

---
+++ _next/static/y49OE2E5ykrgdSyR4DNHp/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fchapters\u002F[id]","\u002Fletters\u002F[id]","\u002Fmemory\u002F[id]","\u002Fmoments\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
